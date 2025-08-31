4bbb0f89d330d67dcb1f355ac1df3f3698749b1c
ariqbal09-stack <ar.iqbal09@gmail.com>
2025-08-31 15:08:51 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'hrv24-v1';
-const files = ['/', 'index.html', 'manifest.json', 'icon.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(files))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
