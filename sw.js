e9a991caf082ea4a16f852c944c8c6e38b3e3b55
ariqbal09-stack <ar.iqbal09@gmail.com>
2025-08-31 12:54:10 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = 'hrv24-v1'; 
-const urlsToCache = ['/hrv24-app/', '/hrv24-app/index.html', '/hrv24-app/manifest.json', '/hrv24-app/icon-192.png']; 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)))); 
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)))); 
