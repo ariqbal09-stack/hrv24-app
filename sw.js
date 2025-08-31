21fad85e3abbfe5ed967b0c43dd2a415c24c1cc1
ariqbal09-stack <ar.iqbal09@gmail.com>
2025-08-31 10:51:59 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'hrv24-v1'; 
+const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png']; 
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)))); 
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)))); 
