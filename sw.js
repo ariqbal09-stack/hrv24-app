42244ffa1fab835e6679683b58a7fbae04dee6cb
ariqbal09-stack <ar.iqbal09@gmail.com>
2025-08-31 12:57:42 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'hrv24-final-v1'; 
+const urlsToCache = ['/hrv24-app/', '/hrv24-app/index.html', '/hrv24-app/manifest.json', '/hrv24-app/icon-192.png']; 
+self.addEventListener('install', e => { 
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); 
+  self.skipWaiting(); 
+}); 
+self.addEventListener('activate', e => { 
+  e.waitUntil(self.clients.claim()); 
+}); 
+self.addEventListener('fetch', e => { 
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request))); 
+}); 
